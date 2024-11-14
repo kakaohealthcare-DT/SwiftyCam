e6e1a03406b963261cca856cfe4659c5802f62be
brunel.lee <brunel.lee@kakaohealthcare.com>
2024-11-14 15:21:40 +0900

    Fix Package swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version: 5.9
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyCam"
+    name: "SwiftyCam",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "SwiftyCam", targets: ["SwiftyCam"])
+    ],
+    targets: [
+        .target(name: "SwiftyCam", path: "Source")
+    ]
 )
